3517cbd1dd1b0646ebda2cca597e967997e149db
Othman Tenich <otenich@gmail.com>
2020-12-04 22:56:53 +0100

    Day 4 Part 2

--- src/main/kotlin/com/namhto/aoc/day4.kt
+++ src/main/kotlin/com/namhto/aoc/day4.kt
@@ -24,14 +24,18 @@ private val fields: Map<String, (v: String) -> Boolean> = mapOf(
     "iyr" to { it.length == 4 && it.toInt() >= 2010 && it.toInt() <= 2020 },
     "eyr" to { it.length == 4 && it.toInt() >= 2020 && it.toInt() <= 2030 },
     "hgt" to {
-        if (it.contains("cm")) {
-            val value = it.substringBefore("cm")
-            value.length == 3 && value.toInt() >= 150 && value.toInt() <= 193
-        } else if (it.contains("in")) {
-            val value = it.substringBefore("in")
-            value.length == 2 && value.toInt() >= 59 && value.toInt() <= 76
-        } else {
-            false
+        when {
+            it.contains("cm") -> {
+                val value = it.substringBefore("cm")
+                value.length == 3 && value.toInt() >= 150 && value.toInt() <= 193
+            }
+            it.contains("in") -> {
+                val value = it.substringBefore("in")
+                value.length == 2 && value.toInt() >= 59 && value.toInt() <= 76
+            }
+            else -> {
+                false
+            }
         }
     },
     "hcl" to { Regex("#[0-9a-f]{6}").matches(it) },
